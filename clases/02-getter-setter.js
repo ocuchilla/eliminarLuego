3665d7f9d2c32a52117108cd704dcc4e19134e88
ocuchilla <ocuchillamaitan@gmail.com>
2025-02-20 10:25:23 -0400

    sexto commit

--- clases/02-getter-setter.js
+++ clases/02-getter-setter.js
@@ -4,4 +4,29 @@ class Vehiculo{
         this.marca=marca;
         this.puerta= puerta
     }
+
+    arrancar(){
+        console.log("quito el neutro y arranco");
+    }
+
+    static frenar(){
+        return ("Freno de mano")
+    }
+
+    set setColor(color){
+        return this.color= color
+    }
+
+    get gertColor(){
+        return this.color;
+    }
+
 }
+
+
+console.log(Vehiculo.frenar());
+
+const modeloFord= new Vehiculo("rojo", "ford", 2);
+console.log(modeloFord.gertColor);
+modeloFord.setColor= "amarillo";
+console.log(modeloFord.gertColor);
